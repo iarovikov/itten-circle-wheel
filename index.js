5644219c09cc0585021f5a46ff06b77b6e4e5cf6
Tima <haiku9167@gmail.com>
2018-08-22 01:46:40 +0600

    dsg

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
 function draw() {
-  var img = document.getElementById("photo");
-  img.onload = function() {
-    context.drawImage(img, 10, 10);
-  };
+  var img = new Image();
   img.src = "Pixel-Art-Mario.jpg";
+  var canvas = document.getElementById("photo");
+  var context = canvas.getContext('2d');
+  img.onload = function () {
+    context.drawImage(img, 0, 0);
+  };
+  var color = document.getElementById('color');
+  function pick(event) {
+    var x = event.layerX;
+    var y = event.layerY;
+    var pixel = context.getImageData(x, y, 1, 1);
+    var data = pixel.data;
+    var rgba = 'rgba(' + data[0] + ', ' + data[1] + ', ' + data[2] + ', ' + (data[3] / 255) + ')';
+    color.textContent = rgba;
+    console.log(color.textContent);
   }
+  canvas.addEventListener('mousedown', pick);
+}
 
